b1baa0bbb19d4e9a901bfee0a4a494937f926eff
Guilherme Henrique Couto de Jesus <guicoutoo@outlook.com>
2022-06-26 17:00:18 -0300

    [x] Refatorado algumas pastas e feito footer, faltando corrigir SignUp

--- react-portifolio/src/App.js
+++ react-portifolio/src/App.js
@@ -2,13 +2,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/pages/Home";
+import Products from "./components/pages/Products";
+import Services from "./components/pages/Services";
+import SignUp from "./components/pages/SignUp";
 
 function App() {
   return (
     <BrowserRouter>
-    <Navbar />
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/products" element={<Products />} />
       </Routes>
     </BrowserRouter>
   );
--- react-portifolio/src/components/pages/Home.js
+++ react-portifolio/src/components/pages/Home.js
@@ -3,12 +3,14 @@ import HeroSection from "../HeroSection/HeroSection";
 
 import React from "react";
 import Cards from "../Cards/Cards";
+import Footer from "../Footer/Footer";
 
 function Home() {
   return (
     <>
       <HeroSection />
       <Cards/>
+      <Footer/>
     </>
   );
 }
